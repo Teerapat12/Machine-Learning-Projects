cb713b314beb0027e0dd89e9d2f1be63b39ee2b2
Teerapat12 <teerapat.time12@gmail.com>
2017-03-02 16:08:22 +0700

    nn
    das

--- Kaggle/Dogs vs. Cats Redux Kernels Edition/cnn_dogcat.py
+++ Kaggle/Dogs vs. Cats Redux Kernels Edition/cnn_dogcat.py
@@ -51,12 +51,12 @@ train_datagen = ImageDataGenerator(
 
 test_datagen = ImageDataGenerator(rescale=1./255)
 
-training_set = train_datagen.flow_from_directory('dataset/training_set',
+training_set = train_datagen.flow_from_directory('dataset/train',
                                                  target_size=(64, 64),
                                                  batch_size=32,
                                                  class_mode='binary')
 
-test_set = test_datagen.flow_from_directory('dataset/test_set',
+test_set = test_datagen.flow_from_directory('dataset/test',
                                             target_size=(64, 64),
                                             batch_size=32,
                                             class_mode='binary')
@@ -64,5 +64,6 @@ test_set = test_datagen.flow_from_directory('dataset/test_set',
 classifier.fit_generator(training_set,
                          samples_per_epoch=8000,
                          nb_epoch=25,
-                         validation_data=test_set,
+                         #validation_data=test_set,
                          nb_val_samples=2000)
+
